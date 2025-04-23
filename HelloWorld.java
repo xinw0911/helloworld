c09bec3761826765f3cf96fd049f150536f978df
xinw0911 <cindywanghugetooth@gmail.com>
2025-04-23 15:25:10 -0700

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -36,5 +36,6 @@ public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello, World");
     System.out.println("Hello, World");
+    System.out.println("Hello, World");
   }
 }
